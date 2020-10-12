1ba76da13ea43133916e1296b746436df78abdb0
angelo1104 <madhav1004@icloud.com>
2020-10-12 13:09:14 +0000

    made post work

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import Flask , request
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello"
+
+@app.route('/model',methods=['POST','GET'])
+def model():
+    if request.method == 'POST':
+        return request.get_json()
+    
+
+if __name__ == '__main__':
+    app.run()
