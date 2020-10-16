4c837a72085e6fa8f00120a2caa3c8d9c917af1b
angelo1104 <madhav1004@icloud.com>
2020-10-16 09:27:13 +0000

    model predicts yay

--- app.py
+++ app.py
@@ -1,16 +1,62 @@
-from flask import Flask , request
+from flask import Flask , request ,jsonify
 import json
+import pickle
+import numpy as np
+from flask_cors import CORS
+
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 5)
+
 
 app = Flask(__name__)
+
+cors = CORS(app)
 
 @app.route('/')
 def home():
     return "Hello"
 
+
 @app.route('/model',methods=['POST','GET'])
 def model():
+    model = pickle.load(open('model.sav','rb'))
+    myList = []
+
+
     if request.method == 'POST':
-        return request.get_json()
+
+        response = request.get_json()
+        print(response)
+        pregnancies = response.get('pregnancies')
+        myList.append(float(pregnancies))
+        glucose = response.get('glucose')
+        myList.append(float(glucose))
+        bloodPressure = response.get('bloodPressure')
+        myList.append(float(bloodPressure))
+        skinThickness = response.get('skinThickness')
+        myList.append(float(skinThickness))
+        insulin = response.get('insulin')
+        myList.append(float(insulin))
+        bmi = response.get('bmi')
+        myList.append(float(bmi))
+        age = response.get('age')
+        myList.append(float(age))
+        
+        gotList = np.array(myList)
+        gotListPoly = poly_reg.transform(gotList.reshape(1,-1))
+
+
+        predictedValues = model.predict(gotListPoly)
+
+        print(predictedValues)
+        
+        data = {
+            "value": str(predictedValues[0])
+        }
+
+        myList = []
+        return jsonify(data)
     
 
 if __name__ == '__main__':
